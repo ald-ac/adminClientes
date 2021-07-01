eabbf144d3c1c0aee0656e09f3ff962a33ebb07e
ald-ac <muriasaldair@hotmail.com>
2021-06-30 21:44:57 -0500

    Agregado de un cliente a indexedBD

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -27,13 +27,47 @@
         
         //Leer input para valores
         const nombre = document.querySelector('#nombre').value;
-        const correo = document.querySelector('#email').value;
+        const email = document.querySelector('#email').value;
         const telefono = document.querySelector('#telefono').value;
         const empresa = document.querySelector('#empresa').value;
 
         //Que no esten vacios
-        if(nombre === '' || correo === '' || telefono === '' || empresa === '') {
+        if(nombre === '' || email === '' || telefono === '' || empresa === '') {
             imprimirAlerta('Todos los campos son obligatorios', 'error');
+        }
+
+        //Crear objeto literal con info
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        cliente.id = Date.now();
+
+        //Agregar a BD
+        agregarClienteBD(cliente);
+    }
+
+    function agregarClienteBD(cliente) {
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprimirAlerta('Error al agregar cliente', 'error');
+        }
+
+        transaction.oncomplete = function() {
+            imprimirAlerta('Cliente agregado');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000)
         }
     }
 
